d5a4e250f4649c06a3e830575b0f4684875bb885
Satoaki Ishihara <satoakiishihara@sd.taylors.edu.my>
2022-11-14 22:24:34 +0900

    satoaki's first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Please enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
+
+        if (userInput % 2 != 0){
+            System.out.println("The number is odd.");
+        } else if (userInput % 2 == 0){
+            System.out.println("The number is even.");
+        }
+    }
+}
