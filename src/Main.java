7dc523a3365f6de53cc780fbccda0afa1205c358
satoaki_github <satoakiishihara@sd.taylors.edu.my>
2022-11-14 22:33:04 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+/**
+ * class satoaki's main
+ */
 public class Main {
     public static void main(String[] args){
         System.out.print("Please enter the number: ");
